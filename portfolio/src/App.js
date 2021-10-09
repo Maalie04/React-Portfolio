bb088ce62e24673fef68d00c116c050f5e6702aa
Jamaal Lawson <maaliemaal@gmail.com>
2021-10-09 15:48:59 -0400

    deleted node modules, then added a new components home.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,39 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 import Portfolio from "./components/Portfolio";
 import Resume from "./components/Resume";
-import './App.css';
+import Home from "./components/Home";
+import "./App.css";
 
 function App() {
-  const [currentPage, setCurrentPage] = useState('Home');
+  const [currentPage, setCurrentPage] = useState("Home");
 
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
-    if (currentPage === 'About') {
+    if (currentPage === "Home") {
+      return <Home />;
+    }
+    if (currentPage === "About") {
       return <About />;
     }
-    if (currentPage === 'Blog') {
+    if (currentPage === "Portfolio") {
       return <Portfolio />;
     }
-    if (currentPage === 'Blog') {
+    if (currentPage === "Contact") {
       return <Contact />;
     }
-    if (currentPage === 'Blog') {
+    if (currentPage === "Resume") {
       return <Resume />;
     }
   };
 
   const handlePageChange = (page) => setCurrentPage(page);
 
-  return(
+  return (
     <div className="App">
       <Navbar
-      currentPage={currentPage}
-      handlePageChange={handlePageChange}
-></Navbar>
-{renderPage()}
+        currentPage={currentPage}
+        handlePageChange={handlePageChange}
+      ></Navbar>
+      {renderPage()}
     </div>
   );
 }
