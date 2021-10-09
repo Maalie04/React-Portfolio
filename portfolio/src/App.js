a1528f7fbef37104c9ae9b1cec88cc71ac6d013a
Jamaal Lawson <maaliemaal@gmail.com>
2021-10-09 14:41:58 -0400

    adding components

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,19 @@
-
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Portfolio from "./components/Portfolio";
+import Resume from "./components/Resume";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    <h1> Hello </h1>
-    </div>
+  <div className="App"> 
+   <About></About>
+   <Contact></Contact>
+   <Navbar></Navbar>
+   <Portfolio></Portfolio>
+   <Resume></Resume>
+   </div>
   );
 }
 
---
+++ portfolio/src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <div> 
+        <h1> Navbar Page </h1>
+        </div>
+    );
+}
