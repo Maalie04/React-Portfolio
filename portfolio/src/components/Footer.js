b756d38e0701662537183fb78b32817b856ea522
Jamaal Lawson <maaliemaal@gmail.com>
2021-10-11 08:52:10 -0400

    added the footer

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -4,7 +4,14 @@ export default function Footer(){
     return (
 
         <div>
-            <h1>Footer</h1>
+            <div className='container'>
+                <footer className='d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top'>
+                    <div className='col-md-4 d-flex align-items-center'>
+                        <span className='span-text'>Jamaal Lawson</span>
+                    </div>
+                </footer>
+            </div>
+            
         </div>
-    )
+    );
 }
