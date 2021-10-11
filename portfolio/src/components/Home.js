9caedfcf45c3f4ca40d6034b9f69c6da5b23db26
Jamaal Lawson <maaliemaal@gmail.com>
2021-10-11 11:56:48 -0400

    added a css folder to style the home, navbar and portfolio page

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import '../css/Home.css';
 
-export default function Home() {
+
+export default function Home(props) {
+    useEffect(() => {
+        document.title = `${props.title}`;
+    });
     return (
         <div> 
-        <h1>Home Page </h1>
+        <h1 className="display-5 fw-bold home-title">Jamaal Lawson  </h1>
         </div>
     );
 }
--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar({currentPage, handlePageChange}) {
   return (
     <div>
       <div className='navbar'>
-        <a className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'} href='#Home' onClick={() => handlePageChange('Home')}>Home</a>
+        {/* <a className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'} href='#Home' onClick={() => handlePageChange('Home')}>Home</a> */}
         <a className={currentPage === 'About' ? 'nav-link active' : 'nav-link'} href='#About' onClick={() => handlePageChange('About')}>About</a>
         <a className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'} href='#Portfolio' onClick={() => handlePageChange('Portfolio')}>Portfolio</a>
         <a className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'} href='#Resume' onClick={() => handlePageChange('Resume')}>Resume</a>
