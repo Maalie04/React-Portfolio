1ca75e9ddd166d873e646fd361e6b8fe0c287079
Jamaal Lawson <maaliemaal@gmail.com>
2021-10-11 12:32:34 -0400

    added a portfolio card component to show project applications

--- portfolio/src/components/Portfolio.js
+++ portfolio/src/components/Portfolio.js
@@ -1,9 +1,30 @@
-import React, from "react";
+import React,{ useEffect } from "react";
+import PortfolioCard from './PortfolioCard';
 
-export default function Portfolio() {
+const p
+
+
+export default function Portfolio(props) {
+    useEffect(() => {
+        document.title = `${props.title}`;
+    });
     return (
         <div> 
-        <h1> Portfolio Page </h1>
+        <h1 className='display-5 fw-bold'>{props.title} Portfolio Page </h1>
+        <div className='container'>
+            <p>
+                React Portfolio
+            </p>
+            <div className='row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3'>
+            {portfolioItems.map((site) => (
+						<PortfolioCard
+							title={site.title}
+							repo={site.repo}
+							deployed={site.deployed}
+						></PortfolioCard>
+					))}
+            </div>
+        </div>
         </div>
     );
 }
