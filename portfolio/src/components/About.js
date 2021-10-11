05e73230eb5e95af99eac416b0c9b2379c05fd63
Jamaal Lawson <maaliemaal@gmail.com>
2021-10-11 10:11:37 -0400

    added the about page still need to add pic and about me text

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-export default function About() {
+export default function About(props) {
+    useEffect(() => {
+        document.title = `${props.title}`;
+    });
     return (
         <div> 
-        <h1> About Page </h1>
+          <div className="px-4 py-5 my-5 text-center">
+				<img
+					className="profile-pic d-block mx-auto mb-4"
+					src=""
+					alt=""
+				/>
+				<h1 className="display-5 fw-bold">About</h1>
+				<div className="col-lg-6 mx-auto">
+            <p>
+
+
+
+
+            </p>
+        </div>
+        </div>
         </div>
     );
 }
