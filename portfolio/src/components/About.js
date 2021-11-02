a0ac0cb0c71509a21cfa970bdb0c3aed36b1884c
Jamaal Lawson <maaliemaal@gmail.com>
2021-11-02 17:51:28 -0400

    trying to add pic

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import portfoliopic from  "";
+import portfoliopic from  "../../images/portfoliopic";
 
 export default function About(props) {
   useEffect(() => {
