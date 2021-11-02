c68b9f948e4066ec4cf51af0a8d44b7fa75cced3
Jamaal Lawson <maaliemaal@gmail.com>
2021-11-02 16:52:12 -0400

    added a profile pic

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -1,27 +1,25 @@
 import React, { useEffect } from "react";
+import portfoliopic from "../";
 
 export default function About(props) {
-    useEffect(() => {
-        document.title = `${props.title}`;
-    });
-    return (
-        <div> 
-          <div className="px-4 py-5 my-5 text-center">
-				<img
-					className="profile-pic d-block mx-auto mb-4"
-					src=""
-					alt=""
-				/>
-				<h1 className="display-5 fw-bold">About</h1>
-				<div className="col-lg-6 mx-auto">
-            <p>
-
-
-
-
-            </p>
+  useEffect(() => {
+    document.title = `${props.title}`;
+  });
+  return (
+    <div>
+      <div className="px-4 py-5 my-5 text-center">
+        <img className="picture" src={portfoliopic} alt=""/>
+        <h1 className="display-5 fw-bold">About</h1>
+        <div className="col-lg-6 mx-auto">
+          <p>
+            Hello, my name is Jamaal Lawson and I am a full-stack web developer. With background
+            in management, I learned how to solve problems and be a natural leader. Currently looking to bring my knowledge,
+            communication, and technical skills to an organization
+            while focusing on continuous learning and engaging in my community
+            to give back in the field of web development.
+          </p>
         </div>
-        </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 }
--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -1,17 +1,16 @@
 import React from "react";
+import { TipOverlay } from "./Navagation";
 
-export default function Footer(){
-    return (
-
-        <div>
-            <div className='container'>
-                <footer className='d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top'>
-                    <div className='col-md-4 d-flex align-items-center'>
-                        <span className='span-text'>Jamaal Lawson</span>
-                    </div>
-                </footer>
-            </div>
-            
-        </div>
-    );
+export default function Footer() {
+  return (
+    <div>
+      <div className="container">
+        <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+          <div className="col-md-4 d-flex align-items-center">
+            <span className="span-text">Jamaal Lawson</span>
+          </div>
+        </footer>
+      </div>
+    </div>
+  );
 }
