622e0e93f4a75fdb541ae7c8281e3cb2066e120d
Jamaal Lawson <maaliemaal@gmail.com>
2021-11-07 12:07:10 -0500

    adding resume pdf file

--- portfolio/src/components/Pages/Resume.js
+++ portfolio/src/components/Pages/Resume.js
@@ -3,7 +3,18 @@ import React from "react";
 export default function Resume() {
     return (
         <div> 
-        <h1> Resume Page </h1>
+            <div className="px-4 py-5 my-5 text-center">
+
+        <h1 className="display-5 fw-bold"> Resume Page </h1>
+        <div className="col-lg-6 mx-auto"> 
+        <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+
+        <a href="Jamaal_Lawson_Resume_2.pdf" download="Jamaal_Lawson_Resume_2.pdf" className="btn btn-outline-secondary btn-lg px-4">
+              Download PDF
+            </a>
+					</div>
+				</div>
+			</div>
         </div>
     );
 }
